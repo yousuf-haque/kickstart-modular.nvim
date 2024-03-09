96fec5b055c80a36a08a23e03bc72e37bba98380
Yousuf Haque <yousuf.s.haque@gmail.com>
2024-03-09 18:34:15 -0500

    Search hidden files with telescope

--- lua/kickstart/plugins/telescope.lua
+++ lua/kickstart/plugins/telescope.lua
@@ -60,13 +60,20 @@ return {
         --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
         --   },
         -- },
-        -- pickers = {}
+        pickers = {
+          find_files = {
+            hidden = true,
+          },
+        },
         extensions = {
           ['ui-select'] = {
             require('telescope.themes').get_dropdown(),
           },
           file_browser = {
             hijack_netrw = true,
+            hidden = true,
+            grouped = true,
+            respect_gitignore = false,
           },
         },
       }
