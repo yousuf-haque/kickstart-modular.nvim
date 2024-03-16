a576bffd85eed4677304f71351793969b18863e0
Yousuf Haque <yousuf@opuswallet.com>
2024-03-16 14:08:48 -0400

    Add nvim treesitter context

--- lua/kickstart/plugins/treesitter.lua
+++ lua/kickstart/plugins/treesitter.lua
@@ -29,5 +29,8 @@ return {
       --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
     end,
   },
+  {
+    'nvim-treesitter/nvim-treesitter-context',
+  },
 }
 -- vim: ts=2 sts=2 sw=2 et
