570b25869dcef27b736d8c60a990ca83752b95c0
Yousuf Haque <yousuf.s.haque@gmail.com>
2025-03-08 17:44:56 -0500

    Set prettier as typescript and javascript formatter

--- lua/kickstart/plugins/conform.lua
+++ lua/kickstart/plugins/conform.lua
@@ -35,7 +35,9 @@ return {
         -- python = { "isort", "black" },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
-        -- javascript = { "prettierd", "prettier", stop_after_first = true },
+        javascript = { 'prettier', stop_after_first = true },
+        typescript = { 'prettier' },
+        typescriptreact = { 'prettier' },
       },
     },
   },
